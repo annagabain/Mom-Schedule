29ef2db73fc0ba8d4ffa7c1cd9801a63d4c8cd62
Anna Gabain <anna.gabain@outlook.com>
2023-02-11 15:30:23 +0000

    Hide the extra register and login buttons

--- mom_schedule_app/views.py
+++ mom_schedule_app/views.py
@@ -47,7 +47,7 @@ def login_request(request):
             if user is not None:
                 login(request, user)
                 messages.info(request, f"You are now logged in as {username}.")
-                return redirect("index")
+                return redirect("all_tasks")
             else:
                 messages.error(request, "Invalid username or password.")
         else:
