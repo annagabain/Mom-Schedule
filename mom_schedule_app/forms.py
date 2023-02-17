507bcee9c7ffab1b51a2e7af3bc9ffe37bfd75ac
Anna Gabain <anna.gabain@outlook.com>
2023-02-17 12:27:29 +0000

    Style the forms and anchor tags

--- mom_schedule_app/forms.py
+++ mom_schedule_app/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class NewUserForm(UserCreationForm):
 	email = forms.EmailField(required=True)
-
+	
 	class Meta:
 		model = User
 		fields = ("username", "email", "password1", "password2")
