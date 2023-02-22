8f59a74bed74f5a08a8b615a5e0dbece5eacd5d0
Anna Gabain <anna.gabain@outlook.com>
2023-02-22 15:55:18 +0000

    Add category dummy field to the new task form

--- mom_schedule_app/models.py
+++ mom_schedule_app/models.py
@@ -11,25 +11,25 @@ class Mom_task(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="momtask")  # noqa
     title = models.CharField(max_length=50, null=False, blank=False)
     complete = models.BooleanField(null=False, blank=False, default=False)
-    # starred
     # category = models.ForeignKey(Mom_category, null=True, on_delete=models.CASCADE, related_name="momtask")  # noqa
     description = models.TextField(max_length=500)
-    # date = models.DateField(widget=DateInput(format='%Y-%m-%d', attrs={'type': 'date'}), required=False)  # noqa
     date = models.DateField()
-    # date_due = models.DateTimeField()
 
     def __str__(self):
         return self.title
 
 
-# # PLACEHOLDER for Category table
+# Category table
 # class Mom_category(models.Model):
-#     id
-#     user_id (fk)
-#     work
-#     home
-#     private_time
-#     other
+#   name = models.CharField(max_length=50, null=False)
+    # def __str__(self):
+    #     return self.name
+
+    # Household
+    # Work
+    # Childcare
+    # Private_time
+    # Other
 
 
 # Contact Form table
